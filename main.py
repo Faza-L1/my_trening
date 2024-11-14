5869c18a4e7d547b6581ca792ab506f24e4b553f
Faza-L1 <85bondarenko@gmail.com>
2024-11-14 22:59:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+print("1st program")
+print((9**0.5)*5)
+print("2st program")
+print(9.99>9.98/1000!=1000.1)
+print("3st program")
+print(2+2*2)
+print(2*(2+2))
+a=6
+b=8
+print(a==b)
+print("4st program")
+print(int(1234.5610%10))
